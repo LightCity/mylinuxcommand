22cf1d1efc3ad39200464ac623883568b9beb5cd
Longli <longcpp@gmail.com>
2016-01-26 21:03:17 +0800

    2016年 1月26日 星期二 21时03分17秒 CST

--- fuck.sh
+++ fuck.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
-git init
-git add fuck.sh
-git commit -m "first commit"
-git remote add origin git@github.com:LightCity/mylinuxcommand.git
-git push -u origin master
+date=`date`
+git add .
+git commit -m "$date"
+git push
+#Mac 使用：将当前目录的文件（不是文件夹）全部移到子目录下
+#find . -type f -maxdepth 1 | xargs -I {} mv {} movie/
+
+
